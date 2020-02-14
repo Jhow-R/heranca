34623df94805f21808f6b84371d45f5f6fc55a28
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2020-02-14 13:45:12 -0300

    Final implementation

---
+++ Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca.Entities
+{
+    // sealed: a classe SavingsAccount não pode ser herdada como Account
+    sealed class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalace()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        // sealed: Withdraw() não pode ser sobrescrita novamente se SavingsAccount fosse herdada em outra classe
+        public sealed override void Withdraw(double amount)
+        {
+            // base: Acessa a implementanção da superclasse
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,49 @@
+﻿using Heranca.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500.00, 400.0));
+            list.Add(new SavingsAccount(1003, "Bob", 500.00, 0.01));
+            list.Add(new BusinessAccount(1004, "Ana", 500.00, 500.0));
+
+            double sum = 0.0;
+
+            foreach (Account acc in list)
+            {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach (Account acc in list)
+            {
+                // Override no método Withdraw, dependendo do tipo de conta (Savings ou Business), ocorrerá diferentes taxas
+                acc.Withdraw(10.0);
+            }
+
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Updated balance for account "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
